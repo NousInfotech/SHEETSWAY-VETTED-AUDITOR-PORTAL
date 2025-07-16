2d137d655d244909a065df4b526d6a363f04ae35
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-07-16 19:29:40 +0530

    updated redirection to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,48 @@
+// 'use client';
+// import { useAuth } from '@/components/layout/providers';
+// import { redirect } from 'next/navigation';
+// import { useEffect } from 'react';
+// // Removed unused React import
+
+// export default function Dashboard() {
+//   const { user, loading } = useAuth();
+//   useEffect(() => {
+//     if (!loading) {
+//       if (!user) {
+//         redirect('/auth/sign-in');
+//       } else {
+//         redirect('/dashboard/overview');
+//       }
+//     }
+//   }, [user, loading]);
+//   return null;
+// }
+
 'use client';
+
 import { useAuth } from '@/components/layout/providers';
-import { redirect } from 'next/navigation';
+
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-// Removed unused React import
+import { Loader2 } from 'lucide-react';
 
 export default function Dashboard() {
+  const router = useRouter();
   const { user, loading } = useAuth();
+
   useEffect(() => {
     if (!loading) {
       if (!user) {
-        redirect('/auth/sign-in');
+        router.push('/auth/sign-in');
       } else {
-        redirect('/dashboard/overview');
+        router.push('/auth/audit-firm');
       }
     }
-  }, [user, loading]);
-  return null;
+  }, [user, loading, router]);
+
+  return (
+    <div className='flex h-screen w-full items-center justify-center'>
+      <Loader2 className='text-muted-foreground h-10 w-10 animate-spin' />
+    </div>
+  );
 }
