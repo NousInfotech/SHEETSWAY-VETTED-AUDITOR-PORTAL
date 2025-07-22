5757e4851a5de879729b9f9cffd9393ecc4e3815
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-07-23 03:08:14 +0530

    update pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,11 @@ import { useProfileStore } from '@/stores/useProfileStore';
 
 export default function Page() {
   const router = useRouter();
-  const { user, loading } = useAuth();
-  const profile = useProfileStore((state) => state.profile);
 
-  // useEffect(() => {
-  //   if (loading) return;
+  useEffect(() => {
+    router.push('/auth/sign-in');
+  }, [])
 
-  //   if (user && profile) {
-  //     router.push('/dashboard/overview');
-  //   } else if (user && !profile) {
-  //     router.push('/auth/audit-firm');
-  //   } else {
-  //     router.push('/auth/sign-in');
-  //   }
-  // }, [user, profile, loading, router]);
-
-  // Show a loader while the logic runs
-  return (
-    <div className='flex h-screen w-full items-center justify-center'>
-      <Loader2 className='text-muted-foreground h-10 w-10 animate-spin' />
-    </div>
-  );
+  
+  return;
 }
